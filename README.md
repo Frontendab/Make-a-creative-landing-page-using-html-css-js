73d4323d6fb9973060860adb92da05907e485fee
Ayoub Sadouri <ayoubpourletravail@gmail.com>
2023-06-08 18:41:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Make-a-creative-landing-page-using-html-css-js" 
---
+++ js/index.js
@@ -0,0 +1,17 @@
+let img = document.getElementById('img');
+ 
+function changeImg1(phone){
+    img.src = phone;
+}
+
+let container = document.getElementById('container');
+function bgColors(color){
+    container.style.background= color;
+}
+
+
+
+
+
+
+
